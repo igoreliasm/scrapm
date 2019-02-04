8d94d86a901c4b80293feff04c3716dc6cf022a3
Igor Elias <igor.elias.muniz@gmail.com>
2019-02-04 14:57:38 -0200

    tofix lint

--- src/todo/movieForm.jsx
+++ src/todo/movieForm.jsx
@@ -1,37 +1,36 @@
 import React from 'react'
 
 export default props => {
-    const keyHandle = (e) => {
-        if (e.key === 'Enter') {
-            props.handleSearch()
-        }
+  const keyHandle = (e) => {
+    if (e.key === 'Enter') {
+      props.handleSearch()
     }
+  }
 
-    const showDataNumbers = () => {
+  const showDataNumbers = () => {
 
-        if (props.list['total'] || props.list['total'] == 0) {
-            return (
-                <div>
-                    <br />
-                    <span>Results: {props.list['total']} - Pages: {props.list['total_pages']}</span>
-                </div>
-            )
-        }
-    }
-
-    return (
-        <div role='form' className='movieForm'>
-            <div>
-                <input id='movie' className='form-control'
-                    placeholder='Digite um filme e pressione enter!' value={props.movie}
-                    onKeyUp={keyHandle}
-                    onChange={props.handleChange}
-                />
-            </div>
-            <div>
-                {showDataNumbers()}
-            </div>
+    if (props.list['total'] || props.list['total'] == 0) {
+      return (
+        <div>
+          <br />
+          <span>Results: {props.list['total']} - Pages: {props.list['total_pages']}</span>
         </div>
-    )
+      )
+    }
+  }
+
+  return (
+    <div role='form' className='movieForm'>
+      <div>
+        <input id='movie' className='form-control'
+          placeholder='Digite um filme e pressione enter!' value={props.movie}
+          onKeyUp={keyHandle}
+          onChange={props.handleChange}
+        />
+      </div>
+      <div>
+        {showDataNumbers()}
+      </div>
+    </div>
+  )
 }
-    
--- src/todo/movieList.jsx
+++ src/todo/movieList.jsx
@@ -1,37 +1,34 @@
 import React from 'react'
-import ReactLoading from 'react-loading';
 
 import Loader from '../template/loader'
 
 export default props => {
+  const renderMovie = () => {
+    const list = props.list['data'] || []
+    const srcNA = 'https://i.imgur.com/nBsFS8T.jpg'
 
-    const renderMovie = () => {
-
-        const list = props.list['data'] || []
-        const srcNA = 'https://i.imgur.com/nBsFS8T.jpg'
-
-        if (props.hidden) {
-            return (
-                <Loader />
-            )
-        }
-        
-        return list.map(m => (
-            <div className="col-md-4 col-lg-3" key={m.imdbID}>
-                <div className="description">
-                    <img src={m.Poster == 'N/A' ? srcNA : m.Poster} className="poster"/>
-                    <div className="caption">
-                        <h5>{m.Title}</h5>
-                        <p>{m.Year}</p>
-                    </div>
-                </div>
-            </div>
-        ))
+    if (props.hidden) {
+      return (
+        <Loader />
+      )
     }
 
-    return (
-        <div className="row">
-            {renderMovie()}
+    return list.map(m => (
+      <div className='col-md-4 col-lg-3' key={m.imdbID}>
+        <div className='description'>
+          <img src={m.Poster === 'N/A' ? srcNA : m.Poster} className='poster' />
+          <div className='caption'>
+            <h5>{m.Title}</h5>
+            <p>{m.Year}</p>
+          </div>
         </div>
-    )
+      </div>
+    ))
+  }
+
+  return (
+    <div className='row'>
+      {renderMovie()}
+    </div>
+  )
 }
